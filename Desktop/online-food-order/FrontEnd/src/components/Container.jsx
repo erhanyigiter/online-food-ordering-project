7df15549663068cbc6a464809bafcf2bc2e1488d
Erhan <146870949+erhanyigiter@users.noreply.github.com>
2024-08-17 21:43:33 +0300

    Delete Desktop/online-food-order directory

--- Desktop/online-food-order/FrontEnd/src/components/Container.jsx
+++
@@ -1,9 +0,0 @@
-const Container = ({ children, className = "", ...props }) => {
-  return (
-    <div className={`max-w-[1200px] mx-auto p-5 ${className}`} {...props}>
-      {children}
-    </div>
-  );
-}
-
-export default Container;
--- Desktop/online-food-order/FrontEnd/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex justify-center my-6">
-      <div className="loader"></div>
-    </div>
-  );
-};
-
-export default Loader;
